b19b85e7b8956802267a41ec2d03efc0e5e7ba06
JohaRamos <j0hakudai@gmail.com>
2024-10-09 20:28:06 -0500

    V6

--- assets/script.js
+++ assets/script.js
@@ -3,14 +3,14 @@ function checkPassword() {
   const feedback = document.getElementById('feedback');
 
   // Código secreto para desbloquear
-  const secretCode = "infinity"; // Puedes cambiar este código por algo personalizado
+  const secretCode = "infinity"; 
 
   if (passwordInput.toLowerCase() === secretCode) {
     feedback.textContent = "¡Hackeo exitoso!";
     feedback.style.color = "#00ff00";
     
-    // Redirigir a otro HTML cuando el código es correcto
-    window.location.href = "mensaje_cumple.html";  // Cambia "mensaje_cumple.html" por el nombre de tu archivo
+    
+    window.location.href = "mensaje_cumple.html";  
   } else {
     feedback.textContent = "Código incorrecto. ¡Intenta de nuevo!";
     feedback.style.color = "#ff4444";
